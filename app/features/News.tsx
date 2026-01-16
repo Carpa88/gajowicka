0c984770d3fd8608a51b88ab972f0349ed7467e5
Carpa88 <olenka_311@mail.ru>
2026-01-16 13:09:57 +0100

    corrected styles

--- app/features/News.tsx
+++ app/features/News.tsx
@@ -5,14 +5,18 @@ const News = ({ news }: { news: ActivityType[] }) => {
   return (
     <section className="news">
       <h2>Aktualności</h2>
-      <div className="news-grid">
-        {news.map((item) => (
-          <Article key={item.title} data={item} />
+      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-2.5 overflow-hidden">
+        {news.map((item, index) => (
+          <Article key={item.title} data={item} index={index} />
         ))}
       </div>
-      <a href="#" className="more-news">
+      <button
+        type="button"
+        className="inline-block mt-4 font-bold no-underline hover:underline"
+        // onClick={handleLoadMore}
+      >
         Więcej wiadomości →
-      </a>
+      </button>
     </section>
   );
 };
